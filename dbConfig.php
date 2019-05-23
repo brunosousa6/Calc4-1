8a02fe360ff594a13d9c170f54a75d73afb049d8
brunosousa6 <50246786+brunosousa6@users.noreply.github.com>
2019-05-23 15:43:30 -0300

    50

--- dbConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-define ("DB_HOST", "calcdemo.mysql.database.azure.com"); // set database host
-define ("DB_USER", "adm@calcdemo"); // set database user
-define ("DB_PASS","111213calc@"); // set database password
-define ("DB_NAME","login"); // set database name
-
-$link = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die("Couldn't make connection.");
-$db = mysql_select_db(DB_NAME, $link) or die("Couldn't select database");
-?>
