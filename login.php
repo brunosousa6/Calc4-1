fa80302fcc2ad7faf3f3f351b426401cdc32508a
brunosousa6 <50246786+brunosousa6@users.noreply.github.com>
2019-05-23 16:23:18 -0300

    master

--- login.php
+++ login.php
@@ -16,12 +16,7 @@ $result = mysqli_query($conexao, $query);
 
 $row = mysqli_num_rows($result);
 
-if($row == 1) {
-	$_SESSION['usuario'] = $usuario;
-	header('Location: painel.php');
-	exit();
-} else {
-	$_SESSION['nao_autenticado'] = true;
-	header('Location: index.php');
-	exit();
-}
+echo $row;
+echo $usuario;
+echo $senha;exit;
+
